f07e9be2c1851ea52119d64d1a25e541147dffd6
Joshua Jones <jpjones76@users.noreply.github.com>
2019-10-03 01:32:14 -0700

    clarify that sac_shell.sh is not a shell script

--- SAC/sac_shell.sh
+++ SAC/sac_shell.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/bash
-#perf stat -r 10 record -F 99 -a
-# sudo chrt -f 99 /usr/bin/time --verbose `sac sac_q`
-#`sac sac_q`
-
-# *** this is not a shell script. It's a placeholder for example commands.
+# THIS IS NOT A SHELL SCRIPT. It's a set of example commands.
+# NOTE: obtain file 10081701.WVP and copy to the SAC directory if you want the SUDS benchmark
 
 # time
 for i in {1..100}; do (sudo chrt -f 99 perf stat -e task-clock -x ',' bash -c 'export SACAUX=/usr/local/sac/aux; /usr/local/sac/bin/sac sac_suds'); done
 
 # memory
 sudo chrt -f 99 /usr/bin/time -v bash -c 'export SACAUX=/usr/local/sac/aux; /usr/local/sac/bin/sac sac_suds'
+
+# memory trials; memory use is quasi-static so this isn't strictly necessary
+for i in {1..100}; do (sudo chrt -f 99 /usr/bin/time -v bash -c 'export SACAUX=/usr/local/sac/aux; /usr/local/sac/bin/sac sac_suds'); done
