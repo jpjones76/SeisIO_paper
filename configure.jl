1f3fba6772f3b3417b10308748a7d3e1dbc2b321
Joshua Jones <jpjones76@users.noreply.github.com>
2019-10-03 01:34:19 -0700

    add configure.jl

---
+++ configure.jl
@@ -0,0 +1,30 @@
+# Check for needed packages
+using Pkg
+
+function pkg_check(pkgs::Array{String,1})
+  for p in pkgs
+    if get(Pkg.installed(), p, nothing) == nothing
+      Pkg.add(p)
+    else
+      println(p * " found, not installing.")
+    end
+  end
+  return nothing
+end
+
+pkg_check(["BenchmarkTools", "PyPlot", "SeisIO"])
+sacfile = "SAC/benchmarks_sac.csv"
+bench_dir = "Benchmarks"
+
+if isfile(sacfile)
+  println(sacfile * " found, not generating SAC benchmark stats.")
+else
+  include("run_sac_stats.jl")
+end
+
+if isdir("Benchmarks")
+  println(bench_dir * " found, not downloading benchmark data.")
+else
+  import SeisIO: svn_get
+  svn_get("https://github.com/jpjones76/SeisIO-TestData/trunk/Benchmarks", "Benchmarks")
+end
